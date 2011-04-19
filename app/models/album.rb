039dc9ea76eebd0cf8c781ee52a8e36558e0f96b
Júlio Bueno <julioce@gmail.com>
2011-04-19 19:47:47 -0300

    Added small pictures to albuns. Added link_to on h2 noticias on index. Noticias has better typography. Minor CSS improvements on restricted area tables. Uniform show data on restricted area.

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,9 @@ class Album < ActiveRecord::Base
   validates_presence_of :titulo, :descricao, :tipo, :message => "não pode estar em branco"
   
   has_many :images, :dependent => :destroy
+  
+  # Selects the last updated image on the current album
+  def self.find_last_image id
+    Image.find_all_by_album_id(id, :order => 'updated_at DESC', :limit => 1).first
+  end
 end
