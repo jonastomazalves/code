903bce794d13179aad26c92724b03e51c520b7a6
Júlio Bueno <julioce@gmail.com>
2011-04-24 12:18:47 -0300

    Image Magick compression for :small e :medium. Typo fixed on image index view.

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,7 +5,12 @@ class Image < ActiveRecord::Base
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/bmp']
   
-  has_attached_file :photo, :styles => {:small => "120x120>", :medium => "960x480#"}
+  has_attached_file :photo, :styles => { 
+  										:small => {  :geometry => "120x120>", :format => 'JPG'},
+  										:medium => { :geometry => "960x480#", :format => 'JPG'}
+  										},
+  										 			  
+  							:convert_options => { :small => '-quality 50', :medium => '-quality 60' }
   
   belongs_to :album
 end
